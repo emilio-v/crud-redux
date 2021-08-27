3bf57f6fed858eeaac46a6d17250c303786528d8
emilio-v <emilio_785@hotmail.com>
2021-08-27 02:49:27 -0500

    adding routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import Header from './components/Header';
+import Products from './components/Products';
+import NewProduct from './components/NewProduct';
+import EditProduct from './components/EditProduct';
 
 function App() {
     return (
-        <>
+        <Router>
             <Header />
-        </>
+            <div className="container mt-5">
+                <Switch>
+                    <Route exact path="/" component={Products} />
+                    <Route exact path="/new/product" component={NewProduct} />
+                    <Route exact path="/edit/product/:id" component={EditProduct} />
+                </Switch>
+            </div>
+        </Router>
     );
 }
 
