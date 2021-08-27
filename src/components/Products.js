e4fd0acf2999f04e5834786fa51332c2d76d2cf2
emilio-v <emilio_785@hotmail.com>
2021-08-27 03:33:16 -0500

    added edit form and product list table

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,5 +1,18 @@
 const Products = () => {
-    return <h1>Products</h1>;
+    return (
+        <>
+            <h2 className="text-center my-5">Product List</h2>
+            <table className="table table-striped">
+                <thead className="bg-primary table-dark">
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+            </table>
+        </>
+    );
 };
 
 export default Products;
