61e6b8eb6e0c4f4a953b2cf76e163782f6c5f628
emilio-v <emilio_785@hotmail.com>
2021-08-28 01:13:03 -0500

    added a product to the state

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -1,6 +1,28 @@
 import { ADD_PRODUCT, ADD_PRODUCT_SUCCESS, ADD_PRODUCT_ERROR } from '../types';
 
 // Create new products
-export function createNewProductAction(product) {
-    return () => console.log(product);
-}
+export const createNewProductAction = product => {
+    return dispatch => {
+        dispatch(addProduct());
+
+        try {
+            dispatch(addProductSuccess(product));
+        } catch (error) {
+            dispatch(addProductError(true));
+        }
+    };
+};
+
+const addProduct = () => ({
+    type: ADD_PRODUCT,
+});
+
+const addProductSuccess = product => ({
+    type: ADD_PRODUCT_SUCCESS,
+    payload: product,
+});
+
+const addProductError = value => ({
+    type: ADD_PRODUCT_ERROR,
+    payload: value,
+});
--- src/reducers/productsReducer.js
+++ src/reducers/productsReducer.js
@@ -10,6 +10,23 @@ const initialState = {
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (state = initialState, action) => {
     switch (action.type) {
+        case ADD_PRODUCT:
+            return {
+                ...state,
+                loading: true,
+            };
+        case ADD_PRODUCT_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                productos: [...state.productos, action.payload],
+            };
+        case ADD_PRODUCT_ERROR:
+            return {
+                ...state,
+                loading: false,
+                error: true,
+            };
         default:
             return state;
     }
