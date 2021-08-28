dac8127f1ad5337ef5935f7c40f3e973638bfe16
emilio-v <emilio_785@hotmail.com>
2021-08-27 21:25:57 -0500

    installed dev tools, created store, setting redux

---
+++ src/reducers/productsReducer.js
@@ -0,0 +1,13 @@
+// each reducer has its own state
+const initialState = {
+    productos: [],
+    error: null,
+    loading: false,
+};
+
+export default function (state = initialState, action) {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
